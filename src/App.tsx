0f7d0c0011f8e3c85fc13cf8d5dcffe839897f7a
pirinthaban <www.pirinthaban@gmail.com>
2026-04-02 17:44:19 +0530

    Fix GitHub Pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { onAuthStateChanged } from 'firebase/auth';
 import { collection, doc, getDoc, getDocs, limit, query } from 'firebase/firestore';
 import { auth, db } from './firebase';
